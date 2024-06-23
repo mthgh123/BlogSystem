27d285ef19fd56e6d27088ecf0e5c3784a3c5b28
YuXuanjun <571730667@qq.com>
2024-06-23 14:48:55 +0800

    启动事务管理

--- boot/src/main/java/com/codesy/blogsystem/boot/BootApplication.java
+++ boot/src/main/java/com/codesy/blogsystem/boot/BootApplication.java
@@ -2,8 +2,10 @@ package com.codesy.blogsystem.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication(scanBasePackages = "com.codesy.blogsystem")
+@EnableTransactionManagement //启用事务管理
 public class BootApplication {
 
     public static void main(String[] args) {
